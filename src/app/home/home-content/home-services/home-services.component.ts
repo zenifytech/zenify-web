7f9560c3227df6349cee5ea8946e7e18ecaf05ed
Clyde Elarcosa <clyde.elarcosa@gmail.com>
2020-12-09 18:39:29 +0800

    Fixes CSS for 2k and 4k resolutions

--- src/app/home/home-content/home-services/home-services.component.ts
+++ src/app/home/home-content/home-services/home-services.component.ts
@@ -32,18 +32,34 @@ export class HomeServicesComponent implements OnInit {
   public _scrollPosition: number;
 
   private _svgLoaded = [false, false, false, false, false];
+  private _screenWidth: number;
 
   constructor() { }
 
   ngOnInit(): void {
-    this._mobileView = window.innerWidth <= 600;
+    this._screenWidth = window.innerWidth;
+    this._mobileView = this._screenWidth <= 600;
+
+    if (this._screenWidth >= 2560) {
+      this._svgLoaded[0] = true;
+    }
   }
 
   public loadedOnce(index: number, position: number) {
-    if (this._scrollPosition >= position && !this._svgLoaded[index]) {
+    if (this._scrollPosition >= this.getScrollOffset(position) && !this._svgLoaded[index]) {
       this._svgLoaded[index] = true;
     }
     return this._svgLoaded[index];
+  }
+
+  private getScrollOffset(position: number) {
+    if (this._screenWidth >= 3840) {
+      return position + 100;
+    } else if (this._screenWidth >= 2560) {
+      return position - 100;
+    }
+
+    return position;
   }
 
   @HostListener('window:scroll', ['$event']) 
