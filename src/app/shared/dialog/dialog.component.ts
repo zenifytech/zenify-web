542f4d64da7bd471fe2cfd8f546d9c0822d70a8a
Clyde Elarcosa <clyde.elarcosa@gmail.com>
2020-12-09 18:41:32 +0800

    Removes unused messages and query event types

--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -15,15 +15,6 @@ import { ResultDialogComponent } from './result-dialog/result-dialog.component';
 })
 export class DialogComponent implements OnInit {
 
-  private _hideDialogsOnEvent = [
-    QueryEventType.DOMAIN_CHECK,
-    QueryEventType.SIGNIN_PROPRIETOR,
-    QueryEventType.SIGNIN_ASSOCIATE,
-    QueryEventType.FEDERATE_PROPRIETOR,
-    QueryEventType.FEDERATE_ASSOCIATE,
-    QueryEventType.SIGNOUT
-  ]
-
   constructor(
     private dialog: MatDialog,
     private queryEventService: QueryEventService
@@ -37,8 +28,7 @@ export class DialogComponent implements OnInit {
     this.queryEventService.event
       .pipe(untilDestroyed(this))
       .subscribe(event => {
-        if (event && (event.status === QueryEventStatus.COMPLETED || event.status === QueryEventStatus.ERROR)
-          && !this._hideDialogsOnEvent.includes(event.type)) {
+        if (event && (event.status === QueryEventStatus.COMPLETED || event.status === QueryEventStatus.ERROR)) {
           this.dialog.open(ResultDialogComponent, {
             data: {
               header: this.getResultHeader(event.type, event.status),
@@ -52,10 +42,6 @@ export class DialogComponent implements OnInit {
 
   private getResultHeader(type: QueryEventType, status: QueryEventStatus) {
     switch(type) {
-      case QueryEventType.SIGNUP_ASSOCIATE:
-      case QueryEventType.SIGNUP_PROPRIETOR: {
-        return (status === QueryEventStatus.COMPLETED) ? Messages.RESULT_HEADER_SIGNUP_SUCCESS : Messages.RESULT_HEADER_SIGNUP_ERROR;
-      }
       case QueryEventType.SEND_MESSAGE: {
         return (status === QueryEventStatus.COMPLETED) ? Messages.RESULT_HEADER_SEND_INBOX_SUCCESS : Messages.RESULT_HEADER_SEND_INBOX_ERROR;
       }
@@ -64,10 +50,6 @@ export class DialogComponent implements OnInit {
 
   private getResultMessage(type: QueryEventType, status: QueryEventStatus) {
     switch(type) {
-      case QueryEventType.SIGNUP_ASSOCIATE:
-      case QueryEventType.SIGNUP_PROPRIETOR: {
-        return (status === QueryEventStatus.COMPLETED) ? Messages.RESULT_MESSAGE_SIGNUP_SUCCESS : Messages.RESULT_MESSAGE_SIGNUP_ERROR;
-      }
       case QueryEventType.SEND_MESSAGE: {
         return (status === QueryEventStatus.COMPLETED) ? Messages.RESULT_MESSAGE_SEND_INBOX_SUCCESS : Messages.RESULT_MESSAGE_ERROR;
       }
--- src/app/shared/enums/query-event-type.enum.ts
+++ src/app/shared/enums/query-event-type.enum.ts
@@ -1,11 +1,3 @@
 export enum QueryEventType {
-    SEND_MESSAGE = "send_message",
-    DOMAIN_CHECK = "user_domain_check",
-    SIGNUP_PROPRIETOR = "signup_proprietor",
-    SIGNUP_ASSOCIATE = "signup_associate",
-    SIGNIN_PROPRIETOR = "signin_proprietor",
-    SIGNIN_ASSOCIATE = "signin_associate",
-    FEDERATE_PROPRIETOR = "federate_proprietor",
-    FEDERATE_ASSOCIATE = "federate_associate",
-    SIGNOUT = "signout"
+    SEND_MESSAGE = "send_message"
 }
