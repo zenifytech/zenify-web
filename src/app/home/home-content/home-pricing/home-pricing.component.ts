78fa10dd77e61cad5a6c09e1158474989a2d1f2e
Clyde Elarcosa <clyde.elarcosa@gmail.com>
2020-12-19 07:02:04 +0800

    Refactors pricing component

--- src/app/home/home-content/home-pricing/home-pricing.component.ts
+++ src/app/home/home-content/home-pricing/home-pricing.component.ts
@@ -7,90 +7,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePricingComponent implements OnInit {
 
+  public _columns = ['description', 'startup', 'commercial', 'professional', 'enterprise'];
+  public _tierDescriptions = [
+    { description: "Web Pages", startup: "Up to 5", commercial: "Up to 15", professional: "Up to 30", enterprise: "Up to 30++" },
+    { description: "MVP", startup: true, commercial: true, professional: true, enterprise: true },
+    { description: "Company Info Site", startup: true, commercial: true, professional: true, enterprise: true },
+    { description: "Basic Catalogs", startup: false, commercial: true, professional: true, enterprise: true },
+    { description: "Business API", startup: false, commercial: true, professional: true, enterprise: true },
+    { description: "E-Commerce", startup: false, commercial: true, professional: true, enterprise: true },
+    { description: "Third-party APIs", startup: false, commercial: false, professional: true, enterprise: true },
+    { description: "Advanced Data Management", startup: false, commercial: false, professional: true, enterprise: true },
+    { description: "Basic Reports Generation", startup: false, commercial: false, professional: true, enterprise: true },
+    { description: "Complex Algorithms", startup: false, commercial: false, professional: false, enterprise: true },
+    { description: "Advanced Reports Generation", startup: false, commercial: false, professional: false, enterprise: true },
+    { description: "Extensive Data Processing", startup: false, commercial: false, professional: false, enterprise: true },
+    { description: "Full-featured Application", startup: false, commercial: false, professional: false, enterprise: true }
+  ]
+
+  public _tierInclusions = [
+    { description: "Domain Registration", startup: " 1 year", commercial: " 1 year", professional: " 1 year", enterprise: " 1 year" },
+    { description: "Web Hosting", startup: " 1 year", commercial: " 1 year", professional: " 1 year", enterprise: " 1 year" },
+    { description: "SSL Certificate (HTTPS)", startup: " 1 year (DV)", commercial: " 1 year (DV)", professional: " 6 months (OV)", enterprise: " 6 months (OV)" },
+    { description: "Business Emails", startup: " 5 accounts", commercial: " 5 accounts", professional: " 5 accounts", enterprise: " 5 accounts" },
+    { description: "Maintenance", startup: " 2 months", commercial: " 3 months", professional: " 3 months", enterprise: " 6 months" },
+    { description: "Git Repository", startup: true, commercial: true, professional: true, enterprise: true },
+    { description: "Improved SEO", startup: true, commercial: true, professional: true, enterprise: true },
+    { description: "DDOS Protection", startup: true, commercial: true, professional: true, enterprise: true }
+  ]
+
+  public _apicolumns = ['description', 'basic', 'advanced', 'professional', 'enterprise'];
+  public _apiTierDescriptions = [
+    { description: "Monolithic", basic: true, advanced: true, professional: true, enterprise: true },
+    { description: "Microservices", basic: "Up to 2", advanced: "Up to 3", professional: "Up to 5", enterprise: "Up to 5++" },
+    { description: "MVP", basic: true, advanced: true, professional: true, enterprise: true },
+    { description: "Hexagonal Architecture", basic: true, advanced: true, professional: true, enterprise: true },
+    { description: "SQL", basic: true, advanced: true, professional: true, enterprise: true },
+    { description: "NoSQL", basic: true, advanced: true, professional: true, enterprise: true },
+    { description: "Basic CRUD", basic: true, advanced: true, professional: true, enterprise: true },
+    { description: "Basic Authentication", basic: false, advanced: true, professional: true, enterprise: true },
+    { description: "Batch Jobs", basic: false, advanced: true, professional: true, enterprise: true },
+    { description: "Data Export/Import", basic: false, advanced: false, professional: true, enterprise: true },
+    { description: "SSO", basic: false, advanced: false, professional: true, enterprise: true },
+    { description: "Third-party APIs", basic: false, advanced: false, professional: true, enterprise: true },
+    { description: "Complex Algorithms", basic: false, advanced: false, professional: false, enterprise: true },
+    { description: "Extensive Data Processing", basic: false, advanced: false, professional: false, enterprise: true },
+    { description: "Full-featured API", basic: false, advanced: false, professional: false, enterprise: true }
+  ]
+
+  public _apiTierInclusions = [
+    { description: "Server Hosting", basic: " 6 months", advanced: " 6 months", professional: " 6 months", enterprise: " 6 months" },
+    { description: "SSL Certificate (HTTPS)", basic: " 1 year (DV)", advanced: " 1 year (DV)", professional: " 6 months (OV)", enterprise: " 6 months (OV)" },
+    { description: "Git Repository", basic: true, advanced: true, professional: true, enterprise: true },
+    { description: "DDOS Protection", basic: true, advanced: true, professional: true, enterprise: true }
+  ]
+
   public _pricingTiers = [
     {
       name: "Startup",
       minPrice: 2000,
       maxPrice: 8000,
-      currency: "USD",
-      details: [
-        "Up to 5 web pages",
-        "Professional info page",
-        "Company profile page",
-      ],
-      inclusions: [
-        "1 year domain",
-        "1 year web hosting",
-        "1 year SSL certificate",
-        "6 months API server hosting",
-        "Business emails",
-        "SEO",
-        "DDOS Protection",
-      ]
+      currency: "USD"
     },
     {
       name: "Commercial",
       minPrice: 6000,
       maxPrice: 15000,
-      currency: "USD",
-      details: [
-        "Up to 12 web pages",
-        "Basic product catalogs",
-        "Service catalogs",
-        "Small-scale e-Commerce",
-        "Third-party API integrations"
-      ],
-      inclusions: [
-        "1 year domain",
-        "1 year web hosting",
-        "1 year SSL certificate",
-        "6 months API server hosting",
-        "Business emails",
-        "SEO",
-        "DDOS Protection"
-      ]
+      currency: "USD"
     },
     {
-      name: "Professional",
+      name: "Pro",
       minPrice: 12000,
       maxPrice: 25000,
-      currency: "USD",
-      details: [
-        "Up to 30 web pages",
-        "Organization management",
-        "Company asset management",
-        "Large-scale e-Commerce"
-      ],
-      inclusions: [
-        "1 year domain",
-        "1 year web hosting",
-        "1 year SSL certificate",
-        "6 months API server hosting",
-        "Business emails",
-        "SEO",
-        "DDOS Protection",
-      ]
+      currency: "USD"
     },
     {
       name: "Enterprise",
       minPrice: 25000,
       maxPrice: 50000,
-      currency: "USD",
-      details: [
-        "30++ web pages",
-        "Complex data management",
-        "Complex algorithms",
-        "Full-featured applications"
-      ],
-      inclusions: [
-        "1 year domain",
-        "1 year web hosting",
-        "1 year SSL certificate",
-        "6 months API server hosting",
-        "Business emails",
-        "SEO",
-        "DDOS Protection",
-      ]
+      currency: "USD"
     }
   ];
 
@@ -99,60 +93,25 @@ export class HomePricingComponent implements OnInit {
       name: "Basic",
       minPrice: 2000,
       maxPrice: 7000,
-      currency: "USD",
-      details: [
-        "Up to 2 microservices",
-        "Unlimited number of APIs",
-        "SQL/NoSQL",
-        "Basic data queries"
-      ],
-      inclusions: [
-        "1 year hosting",
-        "1 year SSL certificate",
-        "2 months cloud-server hosting",
-        "DDOS Protection"
-      ]
+      currency: "USD"
     },
     {
       name: "Advanced",
-      minPrice: 6000,
+      minPrice: 5000,
       maxPrice: 15000,
-      currency: "USD",
-      details: [
-        "Up to 5 microservices",
-        "Unlimited number of APIs",
-        "SQL/NoSQL",
-        "Advanced data queries",
-        "Authentication",
-        "Authorization",
-        "Third-party API integrations"
-      ],
-      inclusions: [
-        "1 year hosting",
-        "1 year SSL certificate",
-        "3 months cloud-server hosting",
-        "DDOS Protection"
-      ]
+      currency: "USD"
     },
     {
-      name: "Professional",
-      minPrice: 15000,
-      maxPrice: 40000,
-      currency: "USD",
-      details: [
-        "Up to 10 microservices",
-        "Unlimited number of APIs",
-        "Complex data queries",
-        "Complex algorithms",
-        "Complex data processing",
-        "Optimizing resources for existing applications"
-      ],
-      inclusions: [
-        "1 year hosting",
-        "1 year SSL certificate",
-        "3 months cloud-server hosting",
-        "DDOS Protection"
-      ]
+      name: "Pro",
+      minPrice: 12000,
+      maxPrice: 30000,
+      currency: "USD"
+    },
+    {
+      name: "Enterprise",
+      minPrice: 25000,
+      maxPrice: 50000,
+      currency: "USD"
     }
   ]
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { ResultDialogComponent } from './dialog/result-dialog/result-dialog.component';
@@ -35,7 +36,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatSelectModule,
     MatDialogModule,
     MatExpansionModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule
   ],
   exports: [
     HttpClientModule,
@@ -51,7 +53,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatSelectModule,
     MatDialogModule,
     MatExpansionModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule
   ],
 
 })
